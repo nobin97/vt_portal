ea74064b4c74514ea75e818e23a2599190ac9ca2
nobinj <nobinj@qburst.com>
2019-11-04 17:19:31 +0530

    added some changes

--- src/router.js
+++ src/router.js
@@ -8,8 +8,8 @@ import readBlog from "./views/readBlog.vue";
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
-  base: process.env.BASE_URL,
+  // mode: "history",
+  // base: process.env.BASE_URL,
   routes: [
     {
       path: "/",
